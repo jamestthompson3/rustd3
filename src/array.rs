21d2232b70b6146e304390788ef292752327f5c6
Serialdev <carlos.mariscal.melgar@gmail.com>
2018-07-31 15:24:05 +0300

    [+] Generic for mean

--- src/array.rs
+++ src/array.rs
@@ -1,7 +1,63 @@
+#![feature(extern_prelude)]
 /// # Array
 /// Implements d3 array functions ()[https://github.com/d3/d3-array]
 
+extern crate num_traits;
 use std::ops::Div;
+use std::ops::Add;
+use std::convert::*;
+use std::ops::Mul;
+
+pub trait MyInt: Copy + Into<f64> {
+        fn blank();
+
+}
+
+impl MyInt for i32 {
+    fn blank() {
+        unimplemented!()
+    }
+}
+
+impl MyInt for f32 {
+    fn blank() {
+        unimplemented!()
+    }
+}
+
+struct MyStruct<T> where T: MyInt {
+    field: T
+}
+
+impl<T> MyStruct<T> where T: MyInt {
+    fn new(var: T) -> MyStruct<T> {
+        MyStruct {
+            field: var
+        }
+    }
+}
+
+pub fn mean<T>(arr: &[T]) -> f64
+    where T: MyInt
+{
+    let sum_array = arr.iter().fold(0f64 ,|a, &b| a + b.into());
+    let length = arr.len() as f64;
+    sum_array / length
+}
+
+// pub fn sum_array<'a, T: PartialOrd + Copy>(arr: &'static [T]) -> T
+//     where T: std::iter::Sum<&'a T>
+// {
+//     return arr.iter().sum();
+// }
+
+
+
+// pub fn mean<T: PartialOrd + Copy + Div>(arr: &[T]) -> T
+// {
+//     let length = arr.len() as f64;
+//     sum_array(&arr) as f64 / length
+// }
 
 pub fn max<T: PartialOrd + Copy>(arr: &[T]) -> T {
     let mut max = arr[0];
@@ -23,41 +79,29 @@ pub fn min<T: PartialOrd + Copy>(arr: &[T]) -> T {
         max
 }
 
-pub fn extent<'a, T: PartialOrd + Copy>(arr: &[T]) -> Vec<T> {
-    let min = min(arr);
-    let max = max(arr);
-    return vec![min, max];
-}
+// pub fn extent<'a, T: PartialOrd + Copy>(arr: &[T]) -> Vec<T> {
+//     let min = min(arr);
+//     let max = max(arr);
+//     return vec![min, max];
+// }
 
-pub fn sum_array<'a, T: PartialOrd + Copy>(arr: &'static [T]) -> T
-    where T: std::iter::Sum<&'a T>
-{
-    return arr.iter().sum();
-}
+// pub fn deviation<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
+//     variance(arr).sqrt()
+// }
 
-pub fn deviation<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
-    variance(arr).sqrt()
-}
+// fn median<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
+//     1.22
+// }
 
-pub fn mean<T: PartialOrd + Copy + Div>(arr: &[T]) -> f64
-{
-    let length = arr.len() as f64;
-    sum_array(&arr) as f64 / length
-}
-
-fn median<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
-    1.22
-}
-
-pub fn variance<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
-    if arr.len() < 2 {
-            0.0
-        } else {
-        let num: T = arr.iter()
-        .map(|x| {
-            x - mean
-        let denom = (&arr.len() - 1) as f64;
-    (num * num)/denom
-    }
-}
-}
+// pub fn variance<T: PartialOrd + Copy>(arr: &[T]) -> f64 {
+//     if arr.len() < 2 {
+//             0.0
+//         } else {
+//         let num: T = arr.iter()
+//         .map(|x| {
+//             x - mean
+//         let denom = (&arr.len() - 1) as f64;
+//     (num * num)/denom
+//     }
+// }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 extern crate d3_calcs;
 
-use d3_calcs::array::{deviation, extent, max, mean, min, variance};
+// use d3_calcs::array::{deviation, extent, max, mean, min, variance};
+use d3_calcs::array::{max, min, mean};
 
 fn main() {
     let array = vec![
         1.44, 2.23, 3.65, 5.65, 7.66, 7.885, 4.345, 234.33, 2.32, 556.76, 87.69, 0.76, 45.34,
         45.56, 43.44, 45.566, 67.86, 342.34, 344.33, 43.45,
     ];
+    let mean = mean(&array);
     let min = min(&array);
     let max = max(&array);
-    let extent = extent(&array);
-    let variance = variance(&array);
-    let deviation = deviation(&array);
-    let mean = mean(&array);
     println!(
-        "mean is {}\nvariance is {}\ndeviation is {}\nmax is {}\nmin is {}\nextent is {:?}",
-        mean, variance, deviation, max, min, extent
+        "mean is {}\nmax is {}\nmin is {}",
+        mean, max, min
     );
+    // let extent = extent(&array);
+    // let variance = variance(&array);
+    // let deviation = deviation(&array);
+    // println!(
+    //     "mean is {}\nvariance is {}\ndeviation is {}\nmax is {}\nmin is {}\nextent is {:?}",
+    //     mean, variance, deviation, max, min, extent
+    // );
 }
